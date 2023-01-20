b6b794c20710bdd9a679519ca82df1c664cf62a1
Pragyan Shrestha <stha.pragyan.17@gmail.com>
2023-01-20 21:15:07 +0545

    chat initialization moved to service

--- index.js
+++ index.js
@@ -5,31 +5,18 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-import Config from 'react-native-config';
-import {CometChat} from '@cometchat-pro/react-native-chat';
 import messaging from '@react-native-firebase/messaging';
 import {
   ChatNotificaitonHandler,
   NotificationService,
   LocalNotificationServices,
+  chatService,
 } from './src/services/';
 import {IncomingCallScreen} from './src/screens';
 
 LocalNotificationServices.configure();
 
-var appSetting = new CometChat.AppSettingsBuilder()
-  .subscribePresenceForAllUsers()
-  .setRegion(Config.REGION)
-  .build();
-
-CometChat.init(Config.APP_ID, appSetting).then(
-  async () => {
-    console.log('Initialization completed successfully');
-  },
-  error => {
-    console.log('Initialization failed with error:', error);
-  },
-);
+chatService.init();
 
 // listeners for when the user accepts or reject calls when the app is in background or quit state through notification
 ChatNotificaitonHandler.attachCallListeners();
